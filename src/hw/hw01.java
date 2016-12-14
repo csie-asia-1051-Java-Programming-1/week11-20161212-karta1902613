9bb0366e84dcadc846334f877fab9d99994fa512
karta1902613 <105021043@live.asia.edu.tw>
2016-12-14 18:44:41 +0800

    hw 12 1214  1.0
    hw 12 1214  1.0

--- src/hw/hw01.java
+++ src/hw/hw01.java
@@ -1,10 +1,26 @@
 package hw;
 /*
- * Date: 2016/12/12
- * Author: 105021***  ???
+ * Topic:寫個程式幫忙她可以直接把寫好的內容轉化成”加密”的型態。加密結果不會影響原字 母的大小寫，且數字部分亦作相同處理，
+ *       但不處理符號及特殊字元及中文。(第一行為想輸入的內容，不超過 100 個字，第二行為打完你想輸入的內容之後，換 行輸入你想要往後替代的數目)    
+ * Date : 2016/12/12
+ * Author: 105021043 劉凱文
  */
+import java.util.Scanner;
 public class hw01 {
 	public static void main(String[] args) {
-
+		 Scanner scn=new Scanner(System.in);
+		 String n=scn.nextLine();System.out.println(n);
+		 char [] data = n.toCharArray();
+		 for(int i=0;i<data.length;i++){
+			 if((data[i]>=48&&data[i]<=57)||(data[i]>=65&&data[i]<=88)||(data[i]>=97&&data[i]<=120)){
+			     data[i]=(char) (data[i]+2);
+			     System.out.print(data[i]);
+			 }else if(data[i]==121||data[i]==122||data[i]==89||data[i]==90){
+				 data[i]=(char)(data[i]-24);
+				 System.out.print(data[i]);
+			 }else{
+				 System.out.print(data[i]);				 
+			 }
+		 }
 	}
 }
--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -1,10 +1,36 @@
 package hw;
 /*
+ * Topic:一個含 N 個整數的序列中，出現頻率超過 N/2 的整數稱為『過半元素』。寫一個程式求一整數序列是否有過半元素
+ *(一整數序列以空白鍵隔開數字;0 < N < 11; 序列中的數字都是 integer; 輸入包含多 行，一行為一筆測資。若有 過半元素， 輸出該數; 否則輸出 NO)     
  * Date: 2016/12/12
- * Author: 105021***  ???
+ * Author: 105021043 劉凱文
  */
+import java.util.Scanner;
 public class hw02 {
 	public static void main(String[] args) {
-
-	}
+		// TODO Auto-generated method stub
+        Scanner scn=new Scanner(System.in);
+        int time=11;
+        String data[]=new String[time];
+        int x;
+        int t=1;
+        while(time>10){
+    	time=0;
+        String n=scn.nextLine();
+        String data1[]=n.split(" ");
+        time=data1.length;
+         if(time>10){
+    	     System.out.println("只能輸入11個數字以下");
+           }
+           for(int i=0;i<data1.length;i++){
+  	    	 data[i]=data1[i];
+  	       }
+        }   
+        for(int i=0;i<time;i++){
+        	x=Integer.parseInt(data[i]);
+        }
+       
+        		
+     
+        }
 }
